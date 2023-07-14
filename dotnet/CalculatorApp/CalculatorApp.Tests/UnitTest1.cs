e3434b345c82be9ec3b3e54d8541b9b9f800f430
Guilherme Teixeira <4645845+gateixeira@users.noreply.github.com>
2023-07-14 13:53:12 +0200

    Add dotnet folder

---
+++ dotnet/CalculatorApp/CalculatorApp.Tests/UnitTest1.cs
@@ -0,0 +1,159 @@
+using System;
+using Xunit;
+
+namespace CalculatorApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 2;
+            double secondNumber = 3;
+
+            // Act
+            double result = Calculator.Add(firstNumber, secondNumber);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void Subtract_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 5;
+            double secondNumber = 3;
+
+            // Act
+            double result = Calculator.Subtract(firstNumber, secondNumber);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Multiply_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 2;
+            double secondNumber = 3;
+
+            // Act
+            double result = Calculator.Multiply(firstNumber, secondNumber);
+
+            // Assert
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void Divide_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 6;
+            double secondNumber = 3;
+
+            // Act
+            double result = Calculator.Divide(firstNumber, secondNumber);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void Divide_ByZeroThrowsArgumentException()
+        {
+            // Arrange
+            double firstNumber = 6;
+            double secondNumber = 0;
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => Calculator.Divide(firstNumber, secondNumber));
+        }
+
+        [Fact]
+        public void PerformOperation_Add_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 2;
+            double secondNumber = 3;
+            string operation = "+";
+
+            // Act
+            double result = Calculator.PerformOperation(firstNumber, secondNumber, operation);
+
+            // Assert
+            Assert.Equal(5, result);
+        }
+
+        [Fact]
+        public void PerformOperation_Subtract_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 5;
+            double secondNumber = 3;
+            string operation = "-";
+
+            // Act
+            double result = Calculator.PerformOperation(firstNumber, secondNumber, operation);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void PerformOperation_Multiply_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 2;
+            double secondNumber = 3;
+            string operation = "*";
+
+            // Act
+            double result = Calculator.PerformOperation(firstNumber, secondNumber, operation);
+
+            // Assert
+            Assert.Equal(6, result);
+        }
+
+        [Fact]
+        public void PerformOperation_Divide_ReturnsCorrectResult()
+        {
+            // Arrange
+            double firstNumber = 6;
+            double secondNumber = 3;
+            string operation = "/";
+
+            // Act
+            double result = Calculator.PerformOperation(firstNumber, secondNumber, operation);
+
+            // Assert
+            Assert.Equal(2, result);
+        }
+
+        [Fact]
+        public void PerformOperation_UnknownOperationThrowsArgumentException()
+        {
+            // Arrange
+            double firstNumber = 6;
+            double secondNumber = 3;
+            string operation = "%";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => Calculator.PerformOperation(firstNumber, secondNumber, operation));
+        }
+
+        [Fact]
+        public void PerformOperation_DivideByZeroThrowsArgumentException()
+        {
+            // Arrange
+            double firstNumber = 6;
+            double secondNumber = 0;
+            string operation = "/";
+
+            // Act & Assert
+            Assert.Throws<ArgumentException>(() => Calculator.PerformOperation(firstNumber, secondNumber, operation));
+        }
+    }
+}
---
+++ dotnet/CalculatorApp/Program.cs
@@ -0,0 +1,100 @@
+﻿// Get two numbers from the user and display the sum.
+
+using System;
+
+namespace CalculatorApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = 0;
+            double secondNumber = 0;
+            string? operation = null;
+
+            // Get the first number from the user.
+            while (true)
+            {
+                Console.Write("Enter a number: ");
+                if (double.TryParse(Console.ReadLine(), out firstNumber))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            // Get the second number from the user.
+            while (true)
+            {
+                Console.Write("Enter another number: ");
+                if (double.TryParse(Console.ReadLine(), out secondNumber))
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+            }
+
+            // Get an operation from the user.
+            while (true)
+            {
+                Console.Write("Enter an operation (+, -, *, /): ");
+                operation = Console.ReadLine();
+                if (operation == "+" || operation == "-" || operation == "*" || operation == "/")
+                {
+                    break;
+                }
+                Console.WriteLine("Invalid input. Please enter a valid operation.");
+            }
+
+            // Perform the operation.
+            double result = Calculator.PerformOperation(firstNumber, secondNumber, operation);
+
+            // Display the result.
+            Console.WriteLine($"The result is {result}.");
+        }
+    }
+
+    public static class Calculator
+    {
+        public static double PerformOperation(double firstNumber, double secondNumber, string operation)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return Add(firstNumber, secondNumber);
+                case "-":
+                    return Subtract(firstNumber, secondNumber);
+                case "*":
+                    return Multiply(firstNumber, secondNumber);
+                case "/":
+                    return Divide(firstNumber, secondNumber);
+                default:
+                    throw new ArgumentException("Unknown operation.");
+            }
+        }
+
+        public static double Add(double firstNumber, double secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static double Subtract(double firstNumber, double secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public static double Multiply(double firstNumber, double secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static double Divide(double firstNumber, double secondNumber)
+        {
+            if (secondNumber == 0)
+            {
+                throw new ArgumentException("Invalid input. Cannot divide by zero.");
+            }
+            return firstNumber / secondNumber;
+        }
+    }
+}
